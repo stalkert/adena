03916f261f6292e9a9fee390e009884b2a50e6cf
stalkert <taranenko.yura@gmail.com>
2015-11-30 19:46:11 +0200

    add base

---
+++ wp-content/themes/adena/login.php
@@ -0,0 +1,21 @@
+<?php /*
+Template Name: login
+*/
+get_header(); ?> 
+    
+	<div class="container"> 	
+		
+         <div class="row">
+            
+			 <div class="col-md-9" id="item-list">
+				<?php if(have_posts()) : ?>
+				<?php while(have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; ?>			
+				<?php endif; ?>
+             </div>
+                
+        </div>
+
+        </div>
+    <?php get_footer(); ?>  
