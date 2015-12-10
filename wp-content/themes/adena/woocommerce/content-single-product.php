6779c056d303bb1b5f0ecece9ccfae101566d6ab
stalkert <taranenko.yura@gmail.com>
2015-12-10 10:10:02 +0200

    solve problem with sync

--- wp-content/themes/adena/woocommerce/content-single-product.php
+++ wp-content/themes/adena/woocommerce/content-single-product.php
@@ -49,11 +49,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 			 *
 			 * @hooked woocommerce_template_single_title - 5
 			 * @hooked woocommerce_template_single_rating - 10
+			 * @hooked woocommerce_template_single_price - 10
 			 * @hooked woocommerce_template_single_excerpt - 20
 			 * @hooked woocommerce_template_single_add_to_cart - 30
 			 * @hooked woocommerce_template_single_meta - 40
 			 * @hooked woocommerce_template_single_sharing - 50
-			 * @hooked woocommerce_template_single_price - 60
 			 */
 			do_action( 'woocommerce_single_product_summary' );
 		?>
